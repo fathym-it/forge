576ddc3e7f9310b1f317bdf7aa51b6c44d2bf67d
Michael Gearhardt <michael.gearhardt@fathym.com>
2018-12-17 13:31:52 -0700

    Moved data grid to daf-ui

--- projects/ui/src/lib/controls/display/data-grid/config/pagination.config.ts
+++
@@ -1,9 +0,0 @@
-export class PaginationConfig {
-    public pageSize: number;
-    public pageSizeOptions: Array<number>;
-
-    constructor(pageSize: number, pageSizeOptions: Array<number>) {
-     this.pageSize = pageSize;
-     this.pageSizeOptions = pageSizeOptions;
-    }
-   }
--- projects/ui/src/lib/controls/display/data-grid/models/column-config.model.ts
+++
@@ -1,7 +0,0 @@
-export interface IColumnConfigModel {
-    colType: string;
-    title?: string;
-    value?: Function;
-    sortable?: boolean;
-    pipe?: string;
-  }
