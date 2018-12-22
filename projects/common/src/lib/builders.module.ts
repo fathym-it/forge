5daed7c92c86ce80090e06be1a673ad7c41fc7ca
Michael Gearhardt <michael.gearhardt@fathym.com>
2018-12-22 09:54:00 -0700

    Working with all solutions and elements (need to test all elements)

--- projects/common/src/lib/builders.module.ts
+++ projects/common/src/lib/builders.module.ts
@@ -1,14 +1,61 @@
 import { NgModule, ModuleWithProviders } from '@angular/core';
 import { FathymSharedModule } from '@lcu/hosting';
 import { ForgeBuildersService } from './svc/builders.service';
+import { ForgeDepartureTableModule } from '@weather-cloud/lcu-el-departure-table';
+import { ForgeAssetsConfigManagerModule } from '@lowcodeunit/lcu-el-assets-config-manager';
+import { ForgeAssetsDomainInputModule } from '@lowcodeunit/lcu-el-assets-domain-input';
+import { ForgeAssetsDomainViewModule } from '@lowcodeunit/lcu-el-assets-domain-view';
+import { ForgeIdentityRegisterModule } from '@lowcodeunit/lcu-el-identity-register';
+import { ForgeIdentitySignInModule } from '@lowcodeunit/lcu-el-identity-sign-in';
+import { ForgeIdentityRecoverPasswordModule } from '@lowcodeunit/lcu-el-identity-recover-password';
+import { ForgeReportingEditModule } from '@lowcodeunit/lcu-el-reporting-edit';
+import { ForgeReportingViewModule } from '@lowcodeunit/lcu-el-reporting-view';
+import { ForgeDisplayActionModule } from '@lowcodeunit/lcu-el-display-action';
+import { ForgeDisplayChartsModule } from '@lowcodeunit/lcu-el-display-charts';
+import { ForgeDisplayContentModule } from '@lowcodeunit/lcu-el-display-content';
+import { ForgeDisplayJsonViewModule } from '@lowcodeunit/lcu-el-display-json-view';
+import { ForgeDisplayPointerModule } from '@lowcodeunit/lcu-el-display-pointer';
+import { ForgeLayoutFlexModule } from '@lowcodeunit/lcu-el-layout-flex';
+import { ForgeMaterialNavToolbarModule } from '@lowcodeunit/lcu-el-material-nav-toolbar';
+import { ForgeMaterialNavMenuModule } from '@lowcodeunit/lcu-el-material-nav-menu';
+import { ForgeMaterialLayoutGridListModule } from '@lowcodeunit/lcu-el-material-layout-grid-list';
+import { ForgeMaterialLayoutCardModule } from '@lowcodeunit/lcu-el-material-layout-card';
+import { ForgeLayoutSwitchStateModule } from '@lowcodeunit/lcu-el-layout-switch-state';
+import { ForgeLayoutStackedModule } from '@lowcodeunit/lcu-el-layout-stacked';
+
+const modules = [
+	ForgeDepartureTableModule,
+	ForgeAssetsConfigManagerModule,
+	ForgeAssetsDomainInputModule,
+	ForgeAssetsDomainViewModule,
+	ForgeDisplayActionModule,
+	ForgeDisplayChartsModule,
+	ForgeDisplayContentModule,
+	ForgeDisplayJsonViewModule,
+	ForgeDisplayPointerModule,
+	ForgeIdentityRegisterModule,
+	ForgeIdentitySignInModule,
+	ForgeIdentityRecoverPasswordModule,
+	ForgeLayoutFlexModule,
+	ForgeLayoutStackedModule,
+	ForgeLayoutSwitchStateModule,
+	ForgeMaterialLayoutCardModule,
+	ForgeMaterialLayoutGridListModule,
+	ForgeMaterialNavMenuModule,
+	ForgeMaterialNavToolbarModule,
+	ForgeReportingEditModule,
+	ForgeReportingViewModule,
+];
 
 @NgModule({
 	imports: [
 		FathymSharedModule,
+		...modules,
 	],
 	declarations: [
 	],
 	exports: [
+		...modules,
 	]
 })
 export class ForgeBuildersModule {
--- projects/common/src/lib/solutions.module.ts
+++ projects/common/src/lib/solutions.module.ts
@@ -2,13 +2,23 @@ import { NgModule, ModuleWithProviders } from '@angular/core';
 import { FathymSharedModule } from '@lcu/hosting';
 import { ForgeSolutionsService } from './svc/solutions.service';
 import { ForgeWeatherCloudSolutionSolutionModule } from '@weather-cloud/lcu-sln-weather-cloud';
-import { ForgeDepartureTableModule } from '@weather-cloud/lcu-el-departure-table';
 import { ForgeApplicationsSolutionModule } from '@lowcodeunit/lcu-sln-applications';
+import { ForgeAssetsSolutionModule } from '@lowcodeunit/lcu-sln-assets';
+import { ForgeDevicesSolutionModule } from '@lowcodeunit/lcu-sln-devices';
+import { ForgeDashboardsSolutionModule } from '@lowcodeunit/lcu-sln-dashboards';
+import { ForgeIdentitySolutionModule } from '@lowcodeunit/lcu-sln-identity';
+import { ForgeInfrastructureSolutionModule } from '@lowcodeunit/lcu-sln-infrastructure';
+import { ForgeReportingSolutionModule } from '@lowcodeunit/lcu-sln-reporting';
 
 const modules = [
-	ForgeWeatherCloudSolutionSolutionModule,
-	ForgeDepartureTableModule,
 	ForgeApplicationsSolutionModule,
+	ForgeAssetsSolutionModule,
+	ForgeDevicesSolutionModule,
+	ForgeDashboardsSolutionModule,
+	ForgeIdentitySolutionModule,
+	ForgeInfrastructureSolutionModule,
+	ForgeReportingSolutionModule,
+	ForgeWeatherCloudSolutionSolutionModule,
 ];
 
 @NgModule({
