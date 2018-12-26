476f3b84a325068d3ba9194cdfe06d23a40b21f6
Michael Gearhardt <michael.gearhardt@fathym.com>
2018-12-26 13:07:29 -0700

    New Forge Switch script for switching between local dev and prod release

---
+++ .lcu/forge-switch.js
@@ -0,0 +1,35 @@
+(async argsV => {
+  const fs = require("fs-extra");
+
+  const args = require("minimist")(argsV);
+
+  console.log(args);
+
+  const env = args["env"];
+
+  console.log(env);
+
+  const devPath =
+    "C:\\Fathym\\Git\\Apps\\Forge\\Fathym.Forge.Web\\wwwroot\\forge";
+
+  const prodPath = "dist/forge-app";
+
+  const outArgs = {
+    DistPath: env == "prod" ? prodPath : env == "prod" ? devPath : null
+  };
+
+  var angular = await fs.readJSON("angular.json");
+
+  var buildOptions = angular.projects["forge-app"].architect.build.options;
+
+  if (!outArgs.DistPath) {
+    if (buildOptions.outputPath == prodPath) outArgs.DistPath = devPath;
+    else if (buildOptions.outputPath == devPath) outArgs.DistPath = prodPath;
+  }
+
+  buildOptions.outputPath = outArgs.DistPath;
+
+  // angular.projects["forge-app"].architect.build.options = buildOptions;
+
+  await fs.writeJSON("angular.json", angular, { spaces: "\t" });
+})(process.argv.slice(2));
