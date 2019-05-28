103a8af0e3d3e25cffeb6741d8d4b0e6a8b09de1
xchanin <me@shannonbruns.com>
2019-05-28 16:16:35 -0600

    comment out BrowserModule imports

--- projects/sign-in/src/app/core.module.ts
+++ projects/sign-in/src/app/core.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+// import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { FathymSharedModule } from '@lcu/hosting';
@@ -16,7 +16,6 @@ export var modules: any[] = [
 
 @NgModule({
 	imports: [
-		BrowserModule,
 		BrowserAnimationsModule,
 		UIModule.forRoot(),
 		DndModule,
